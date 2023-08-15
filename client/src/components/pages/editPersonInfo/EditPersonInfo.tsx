7838735e8d483d16adf0681b4b19ea0e1d00da60
Aleksandr Zasavitsky <zasavitskya@yandex.ru>
2023-08-15 11:39:01 +0300

    поправил немно редактирование

--- client/src/components/pages/editPersonInfo/EditPersonInfo.tsx
+++ client/src/components/pages/editPersonInfo/EditPersonInfo.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import EditPersonInfoForm from '../../ui/editPersonInfo/EditPersonInfoForm';
+import { editProfileThunk } from '../../../redux/slices/profiles/profileThunk';
+import { useAppDispatch } from '../../../redux/hooks';
 
 export default function EditPersonInfo(): JSX.Element {
-  return <div><EditPersonInfoForm /></div>;
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    void dispatch(editProfileThunk());
+  }, []);
+  return (
+    <div>
+      <EditPersonInfoForm />
+    </div>
+  );
 }
