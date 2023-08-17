43373f0292f99422f5060faf9eb8de7579ca77b0
Aleksandr Zasavitsky <zasavitskya@yandex.ru>
2023-08-17 20:39:13 +0300

    d

--- client/src/components/ui/Post/PostList.tsx
+++ client/src/components/ui/Post/PostList.tsx
@@ -1,39 +1,38 @@
-import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import PostItem from "./PostItem";
-import "./carousel-styles.css"; // Импорт стилей
-import { useAppSelector } from "../../../redux/hooks";
+import React, { useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import PostItem from './PostItem';
+import './carousel-styles.css'; // Импорт стилей
+import { useAppSelector } from '../../../redux/hooks';
 
 export default function PostList(): JSX.Element {
-   const posts = useAppSelector((state) => state.posts.posts);
-   const [activeIndex, setActiveIndex] = useState(0);
-
-   useEffect(() => {
+  const posts = useAppSelector((state) => state.posts.posts);
+  const [activeIndex, setActiveIndex] = useState(0);
+  useEffect(() => {
+    if (!(posts.length === 0)) {
       const interval = setInterval(() => {
-         setActiveIndex((prevIndex) =>
-            (prevIndex + 1) % posts.length
-         );
+        setActiveIndex((prevIndex) => (prevIndex + 1) % posts.length);
       }, 5000); // миллисекунд = 3 секунд
 
-      return () => clearInterval(interval); // Очистка интервала при размонтировании
-   }, [posts.length]);
+      return () => clearInterval(interval);
+    } // Очистка интервала при размонтировании
+  }, [posts.length]);
 
-   return (
-      <Box className="carousel-container">
-         <Carousel
-            showStatus={false}
-            showThumbs={false}
-            selectedItem={activeIndex} // Установка активного элемента
-            stopOnHover={false} // Отключение остановки при наведении
-            infiniteLoop={false} // Отключение бесконечного цикла
-         // swipeable={false} // Отключение прокрутки путем свайпа
-         >
-            {posts.map((post) => (
-               <PostItem key={post.id} post={post} />
-            ))}
-         </Carousel>
-      </Box>
-   )
+  return (
+    <Box className="carousel-container">
+      <Carousel
+        showStatus={false}
+        showThumbs={false}
+        selectedItem={activeIndex} // Установка активного элемента
+        stopOnHover={false} // Отключение остановки при наведении
+        infiniteLoop={false} // Отключение бесконечного цикла
+        // swipeable={false} // Отключение прокрутки путем свайпа
+      >
+        {posts.map((post) => (
+          <PostItem key={post.id} post={post} />
+        ))}
+      </Carousel>
+    </Box>
+  );
 }
